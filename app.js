fdc34dfa4e892e871ce50c489e5e3a0cf6c5e62c
Thar Lin Shwe <albent2017@gmail.com>
2024-10-30 14:03:37 +0800

    Added Vecotr class

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static add(vector1, vector2) {
+        return new Vector(vector1.x + vector2.x, vector1.y + vector2.y);
+    }
+
+    static subtract(vector1, vector2) {
+        return new Vector(vector1.x - vector2.x, vector1.y - vector2.y);
+    }
+
+    scale(scalar) {
+        return new Vector(this.x * scalar, this.y * scalar);
+    }
+
+    magnitude() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    setMagnitude(magnitude) {
+        const currentMagnitude = this.magnitude();
+        if (currentMagnitude === 0) return new Vector(0, 0); // Avoid division by zero
+        const scale = magnitude / currentMagnitude;
+        return this.scale(scale);
+    }
+}
+
 class Chain {
-    constructor(originPos, joinCount, linkSize) {
+    constructor(originPos, joinCount, linkSize, ctx) {
         if (joinCount < 1) {
             throw new Error('Join count must be greater than 1');
         }
 
-        this.joints = new Array(joinCount).fill(null);
-        this.joints[0] = originPos;
         this.angels = [];
         this.linkSize = linkSize;
-
-        for (let i = 1; i < joinCount; i++) {
-            this.joints[i] = {
-                x: this.joints[i - 1].x + this.linkSize,
-                y: this.joints[i - 1].y
-            };
-        }
+        this.ctx = ctx;
+        this.joints = new Array(joinCount).fill(null).map((_, i) => new Vector(originPos.x + (i * linkSize), originPos.y));
     }
 
     display() {
+        const ctx = this.ctx;
+        // drawing the line link
+        for (let i = 1; i < this.joints.length; i++) {
+            ctx.beginPath();
+            ctx.moveTo(this.joints[i - 1].x, this.joints[i - 1].y);
+            ctx.lineTo(this.joints[i].x, this.joints[i].y);
+            ctx.stroke();
+        }
+
+        // drawing the joints
         for (const joint of this.joints) {
             ctx.beginPath();
-            ctx.arc(joint.x, joint.y, this.linkSize, 0, Math.PI * 2);
+            ctx.arc(joint.x, joint.y, 1, 0, Math.PI * 2);
             ctx.stroke();
         }
     }
@@ -58,20 +91,14 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
-const origin = {
-    x: canvas.width / 2,
-    y: canvas.height / 2
-}
 
-const chain = new Chain(origin, 5, 25);
+const origin = new Vector(canvas.width / 2, canvas.height / 2);
+const chain = new Chain(origin, 5, 25, ctx);
 
 canvas.addEventListener('mousemove', (e) => {
     const boundingRect = canvas.getBoundingClientRect();
 
-    const mousePox = {
-        x: e.clientX - boundingRect.left,
-        y: e.clientY - boundingRect.top
-    }
+    const mousePox = new Vector(e.clientX - boundingRect.left, e.clientY - boundingRect.top)
 
     chain.resolve(mousePox);
 });
